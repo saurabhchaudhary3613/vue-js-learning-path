460ac6fa963232aa69ad849120083faa1adb3822
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-06-02 10:57:43 +0530

    updates

---
+++ comunication_between_components/src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{myName}}----{{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: String
+        },
+        methods: {
+            switchName() {
+                return this.myName.split('').reverse().join('');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
