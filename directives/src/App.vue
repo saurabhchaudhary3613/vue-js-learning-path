22691b680f4bda1d77edb8209589d01400121b88
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-06-08 13:57:18 +0530

    directive and forms learning in Vue JS

---
+++ directives/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+                <p v-text="'Some Text !'"></p>
+                <p v-html="'<strong>Some strong text<strong>'"></p>
+            </div>
+            <hr>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custome Directives</h1>
+                <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    //el.style.background = 'green';
+                    //el.style.background = binding.value;
+                    let delay = 0;
+                    if(binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+                    if(binding.modifiers['blink']) {
+                        let mainColor = binding.value;
+                        let secondColor = 'blue';
+                        let currentColor = mainColor;
+                        setTimeout( () => {
+                            setInterval( () => {
+                                currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
+                                if(binding.arg === 'background') {
+                                    el.style.background = currentColor;
+                                } else {
+                                    el.style.color = currentColor;
+                                }
+                            }, 1000);
+                            
+                        }, delay);
+                    }else {
+                        setTimeout( () => {
+                            if(binding.arg === 'background') {
+                                el.style.background = binding.value;
+                            } else {
+                                el.style.color = binding.value;
+                            }
+                        }, delay);
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ forms/src/App.vue
@@ -0,0 +1,173 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <!-- <input
+                                type="text"
+                                id="email"
+                                v-model="userdata.email"
+                                class="form-control"> -->
+                            <input
+                                type="text"
+                                id="email"
+                                :value="userdata.email"
+                                @input="userdata.email = $event.target.value"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                v-model="userdata.password"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                v-model="userdata.age"
+                                class="form-control">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control" v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    v-model="sendEmail"
+                                    value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    v-model="sendEmail"
+                                    value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                v-model="gender"
+                                value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                v-model="gender"
+                                value="Female"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            v-model="selectedPriorty"
+                            class="form-control">
+                        <option v-for="(priorty, index) in priorities" :key="index">{{ priorty }}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button @click.prevent="submitted"
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userdata.email }}</p>
+                        <p>Password: {{ userdata.password }}</p>
+                        <p>Age: {{ userdata.age }}</p>
+                        <p style="white-space: pre">Message: {{message}}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li v-for="(item, i) in sendEmail" :key="i">{{ item }}</li>
+                        </ul>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriorty }}</p>
+                        <p>Switched: {{dataSwitch}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Switch from './switch.vue';
+    export default {
+        data: function() {
+            return {
+                userdata: {
+                    email: '',
+                    password: '',
+                    age: 27
+                },
+                message: 'A text message !',
+                sendEmail: [],
+                gender: 'Male',
+                priorities: ['High', 'Low', 'Medium'],
+                selectedPriorty: 'Low',
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        components: {
+            appSwitch: Switch
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
