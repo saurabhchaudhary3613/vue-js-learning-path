9744f6dadaf6efdc2f523e58c325da99ca9dfa37
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-06-24 14:28:40 +0530

    routing learning

--- routing/src/main.js
+++ routing/src/main.js
@@ -7,8 +7,25 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+
+    if(savedPosition) {
+      return savedPosition;
+    }
+    if(to.hash) {
+      return { selector: to.hash };
+    }
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 });
+router.beforeEach((to, from, next) => {
+  console.log('global before each routing');
+  next();
+})
 
 new Vue({
   el: '#app',
