141b61eb6a9ce4a55fc76aca4f0ce1146bafc1a2
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-06-27 17:10:54 +0530

    vuex understanding

---
+++ vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment({by: 15, duration: 500})">Increment</button>
+        <button class="btn btn-primary" @click="decrement({by: 10, duration: 500})">Decrement</button>
+    </div>
+</template>
+
+<script>
+    // import { mapMutations } from 'vuex';
+    import { mapActions } from 'vuex';
+    export default {
+        methods: {
+            //  ...mapMutations([
+            //     'increment',
+            //     'decrement'
+            // ]),
+            ...mapActions([
+                'increment',
+                'decrement'
+            ])
+            // increment() {
+            //     //this.$emit('updated', 1);
+            //     //this.$store.state.counter++;
+            //     this.$store.commit('increment');
+            // },
+            // decrement() {
+            //     //this.$emit('updated', -1);
+            //     //this.$store.state.counter--;
+            //     this.$store.commit('decrement');
+            // }
+        }
+    }
+</script>
---
+++ vuex/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment(50)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(25)">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { mapMutations } from 'vuex';
+    export default {
+        methods: {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ]),
+            // increment() {
+            //     //this.$emit('updated', 1);
+            //     //this.$store.state.counter++;
+            //     this.$store.commit('increment');
+            // },
+            // decrement() {
+            //     //this.$emit('updated', -1);
+            //     //this.$store.state.counter--;
+            //     this.$store.commit('decrement');
+            // }
+        }
+    }
+</script>
